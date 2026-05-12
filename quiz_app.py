af38b5f3f286fd2ec3d6e1973495501e3e93125f
Mikyas <baherumalik@gmail.com>
2026-05-12 11:28:24 +0300

    Initial commit: Core logic for quiz application

---
+++ quiz_app.py
@@ -0,0 +1,43 @@
+print("*****************************************************")
+print("************** Welcome to the quiz app **************")
+print("*****************************************************")
+
+# Ask for User to play.
+
+playing = input("Do you want to play? ")
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play: ")
+score = 0
+
+answer = input("What does a CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorect")
+
+answer = input("What does a GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorect")
+
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorect")
+
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorect")
+    
+print("You got " + str(score) + " questions correct!")
+print("You got " +  str((score / 4) * 100) + "%.")
